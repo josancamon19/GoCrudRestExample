9201691688cafd5c8c851b0146a889fa1a26ce08
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2020-09-22 14:32:43 -0500

    Users CRUD with gorm + postgreSQL Db

--- main.go
+++ main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"GoCrudRestExample/db"
 	"GoCrudRestExample/models"
+	"fmt"
 	"github.com/gofiber/fiber"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 	"log"
 )
 
+func initDatabase() {
+	var err error
+	dsn := "postgres://plwrwgsqbtghhm:857d60c3c0d54656bc25bf81f3d6edc856b1ed9e80f32fed4ad2ab049df5395b@ec2-54-236-169-55.compute-1.amazonaws.com:5432/d7battfi3i1v7f"
+	db.DBConn, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	err = db.DBConn.AutoMigrate(&models.User{})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Database migrated")
+}
+
 func main() {
+	initDatabase()
 	app := fiber.New()
 
 	app.Get("/users", models.GetUsers)
--- models/user.go
+++ models/user.go
@@ -1,23 +1,26 @@
 package models
 
 import (
+	"GoCrudRestExample/db"
 	"encoding/json"
 	"github.com/gofiber/fiber"
+	"gorm.io/gorm"
 	"net/http"
 	"strconv"
 )
 
-var users = []User{
-	{
-		ID:       1,
-		Name:     "Joan",
-		LastName: "Cabezas",
-		Email:    "joan@concha.abs.com",
-		Age:      19,
-	},
-}
+//var users = []User{
+//	{
+//		ID:       1,
+//		Name:     "Joan",
+//		LastName: "Cabezas",
+//		Email:    "joan@concha.abs.com",
+//		Age:      19,
+//	},
+//}
 
 type User struct {
+	gorm.Model
 	ID       int
 	Name     string
 	LastName string
@@ -26,6 +29,8 @@ type User struct {
 }
 
 func GetUsers(c *fiber.Ctx) error {
+	var users []User
+	db.DBConn.Find(&users)
 	return c.JSON(users)
 }
 
@@ -35,7 +40,7 @@ func CreateUser(c *fiber.Ctx) error {
 	if err != nil {
 		panic(err)
 	}
-	users = append(users, user)
+	db.DBConn.Create(&user)
 	c.Status(http.StatusCreated)
 	return c.JSON(user)
 }
@@ -47,11 +52,12 @@ func GetUserById(c *fiber.Ctx) error {
 		c.Status(http.StatusBadRequest)
 		return c.SendString("")
 	}
-	for _, user := range users {
-		if user.ID == id {
-			return c.JSON(user)
-		}
+	var user User
+	db.DBConn.Find(&user, id)
+	if user.ID != 0 {
+		return c.JSON(user)
 	}
+
 	c.Status(http.StatusNotFound)
 	return c.SendString("User not found")
 }
@@ -63,12 +69,11 @@ func DeleteUserById(c *fiber.Ctx) error {
 		c.Status(http.StatusBadRequest)
 		return c.SendString("")
 	}
-	for i, user := range users {
-		if user.ID == id {
-			users = append(users[:i], users[i+1:]...)
-			c.Status(http.StatusNoContent)
-			return c.SendString("Deleted")
-		}
+	var user User
+	db.DBConn.Find(&user, id)
+	if user.ID != 0 {
+		db.DBConn.Delete(user)
+		return c.SendString("User deleted")
 	}
 	c.Status(http.StatusNotFound)
 	return c.SendString("User not found")
@@ -86,12 +91,16 @@ func UpdateUser(c *fiber.Ctx) error {
 	if err != nil {
 		panic(err)
 	}
-	for i, user := range users {
-		if user.ID == id {
-			users[i] = newUser
-			c.Status(http.StatusOK)
-			return c.SendString("Updated")
-		}
+
+	var user User
+	db.DBConn.Find(&user, id)
+	if user.ID != 0 {
+		user.Name = newUser.Name
+		user.LastName = newUser.LastName
+		user.Email = newUser.Email
+		user.Age = newUser.Age
+		db.DBConn.Save(user)
+		return c.JSON(user)
 	}
 	c.Status(http.StatusNotFound)
 	return c.SendString("User not found")
